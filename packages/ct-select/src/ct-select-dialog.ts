121d5977634c3f481361a8407f5ad3a14ad4237a
Herberth <herberthobregon@gmail.com>
2021-09-21 18:50:35 -0600

    Fix ct-select

--- packages/ct-select/src/ct-select-dialog.ts
+++ packages/ct-select/src/ct-select-dialog.ts
@@ -218,7 +218,7 @@ export class CtSelectDialog extends CtLit {
 				display: block;
 				flex-direction: row;
 				justify-items: flex-end;
-				text-align: initial;
+				text-align: end;
 				font-weight: bold;
 				padding: 16px;
 			}
